77e367f07c90b128b1266db12b749787e45874b7
Akindu Induwara <76662130+AkinduID@users.noreply.github.com>
2025-08-27 20:18:08 +0530

    added main code

--- Lab 1/serial.c
+++ Lab 1/serial.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // Node structure
 struct list_node_s {
@@ -62,4 +63,89 @@ int Delete(int value, struct list_node_s** head_pp){
   } else {
     return 0;
   }
+}
+
+// Helper function to generate a random number in [0, 216-1]
+int get_random_value() {
+  return rand() % 216;
+}
+
+// Helper function to check if value is in the list
+int is_in_list(int value, struct list_node_s* head) {
+  struct list_node_s* curr = head;
+  while (curr != NULL) {
+    if (curr->data == value) return 1;
+    curr = curr->next;
+  }
+  return 0;
+}
+
+int main(int argc, char* argv[]) {
+  int n = 10; // Number of initial unique values
+  int m = 100; // Number of operations
+  float mMember = 0.8, mInsert = 0.1, mDelete = 0.1; // Fractions
+  struct list_node_s* head = NULL;
+  int inserted = 0;
+  int value;
+  int i;
+  int op;
+  int member_count = 0, insert_count = 0, delete_count = 0;
+
+  if (argc >= 5) {
+    n = atoi(argv[1]);
+    m = atoi(argv[2]);
+    mMember = atof(argv[3]);
+    mInsert = atof(argv[4]);
+    mDelete = 1.0f - mMember - mInsert;
+  }
+
+  // Set a different random seed for each execution
+  srand((unsigned int)time(NULL));
+
+  // Populate the list with n unique random values
+  while (inserted < n) {
+    value = get_random_value();
+    if (!is_in_list(value, head)) {
+      Insert(value, &head);
+      inserted++;
+    }
+  }
+
+  // Perform m random operations and measure time
+  clock_t start, end;
+  double cpu_time_used;
+  start = clock();
+  for (i = 0; i < m; i++) {
+    float prob = (float)rand() / RAND_MAX;
+    value = get_random_value();
+    if (prob < mMember) {
+      Member(value, head);
+      member_count++;
+    } else if (prob < mMember + mInsert) {
+      // Only insert if not already in list
+      if (!is_in_list(value, head)) {
+        Insert(value, &head);
+        insert_count++;
+      }
+    } else {
+      Delete(value, &head);
+      delete_count++;
+    }
+  }
+  end = clock();
+  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  printf("Time taken for %d operations: %f seconds\n", m, cpu_time_used);
+
+  printf("Initial n: %d, Total m: %d\n", n, m);
+  printf("Member ops: %d, Insert ops: %d, Delete ops: %d\n", member_count, insert_count, delete_count);
+
+  // Free the list
+  struct list_node_s* curr = head;
+  while (curr != NULL) {
+    struct list_node_s* tmp = curr;
+    curr = curr->next;
+    free(tmp);
+  }
+
+  return 0;
 }
